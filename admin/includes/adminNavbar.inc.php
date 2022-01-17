b0d522ca0d8529f269efaa11dc171df26de48936
skidword64 <74870446+dword64@users.noreply.github.com>
2022-01-17 15:25:29 +0100

    missing .php

--- admin/includes/adminNavbar.inc.php
+++ admin/includes/adminNavbar.inc.php
@@ -4,12 +4,12 @@
 <?php if (Session::isAdmin()) : ?>
 	<div class="col-12 mt-3 mb-2">
 		<div class="rounded p-3">
-			<a href='index' class="btn btn-outline-primary btn-sm">Home</a>
-			<a href='users' class="btn btn-outline-primary btn-sm">Users</a>
-			<a href='invites' class="btn btn-outline-primary btn-sm">License</a>
-			<a href='logs' class="btn btn-outline-primary btn-sm">Logs</a>
+			<a href='index.php' class="btn btn-outline-primary btn-sm">Home</a>
+			<a href='users.php' class="btn btn-outline-primary btn-sm">Users</a>
+			<a href='invites.php' class="btn btn-outline-primary btn-sm">License</a>
+			<a href='logs.php' class="btn btn-outline-primary btn-sm">Logs</a>
 			<a href='sub.php' class="btn btn-outline-primary btn-sm">Sub codes</a>
-			<a href='cheat' class="btn btn-outline-primary btn-sm">cheat</a>
+			<a href='cheat.php' class="btn btn-outline-primary btn-sm">cheat</a>
 		</div>
 	</div>
 <?php endif; ?>
