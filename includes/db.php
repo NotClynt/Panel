a371ca9d95921bd2586ead094fc7e490bfb9fd61
Clynt <73389332+clynt707@users.noreply.github.com>
2022-08-09 09:20:10 +0200

    Delete db.php

--- includes/db.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$mysqli = mysqli_connect("HOST", "DATABASE USER", "DATABASE PASSWORD", "DATABASE NAME");
-if ($mysqli->connect_error) {
-    die('Connect Error: ' . $mysqli->connect_errno . ': ' . $mysqli->connect_error);
-}
